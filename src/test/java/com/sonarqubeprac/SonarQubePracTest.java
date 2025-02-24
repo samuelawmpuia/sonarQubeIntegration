fa2773828a9912f1c39a476618cb5d6df5c66258
Samuel Lalawmpuia <samuellalawmpuia1@gmail.com>
2025-02-24 22:39:30 +0530

    first commit

---
+++ src/test/java/com/sonarqubeprac/SonarQubePracTest.java
@@ -0,0 +1,10 @@
+package com.sonarqubeprac;
+
+import org.junit.jupiter.api.Test;
+
+public class SonarQubePracTest {
+	@Test
+	public void SonarTest() {
+		System.out.println("Hello world");
+	}
+}
